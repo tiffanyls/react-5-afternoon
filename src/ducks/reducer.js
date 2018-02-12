c75e3e8433e558abce0c83f783a61dc4133a430a
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-12 16:21:55 -0600

    finished step 9 and 10

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -20,6 +20,8 @@ email: ''
 const UPDATE_LOAN_TYPE ='UPDATE_LOAN_TYPE';
 const UPDATE_PROPERTY_TYPE = 'UPDATE_PROPERTY_TYPE';
 const UPDATE_CITY = 'UPDATE_CITY';
+const UPDATE_PROP = 'UPDATE_PROP';
+const UPDATE_FOUND = 'UPDATE_FOUND';
 
 export function updateLoanType (loanType) {
     return {
@@ -40,12 +42,28 @@ export function updateCity(city) {
         payload: city
     }
 }
+
+export function updateProp (prop) {
+    return {
+        type: UPDATE_PROP,
+        payload: prop
+    }
+}
+
+export function updateFound(found) {
+    return {
+        type: UPDATE_FOUND,
+        payload: found
+    }
+}
 function reducer(state = initialState, action){
     switch (action.type) {
         case UPDATE_LOAN_TYPE: 
         return Object.assign({}, state, {loanType: action.payload});
         case UPDATE_PROPERTY_TYPE:
         return Object.assign({}, state, {propertyType: action.payload});
+        case UPDATE_PROP:
+        return Object.assign({}, state,{propToBeUsedOn: action.payload});
         default: return state;
     }
 }

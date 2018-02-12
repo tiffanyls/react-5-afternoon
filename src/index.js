bb6d0a754df39e8fb60d7479f6d7f3896588a694
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-12 13:52:31 -0600

    step one and two done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {Provider} from 'react-redux';
+import store from './store';
+import {HashRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<HashRouter><Provider store={store}><App /></Provider></HashRouter>, document.getElementById('root'));
+
 
